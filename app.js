eb508816aa33efcfb2fcbf96f1a985caf8029cb9
VictorB <victormbp2@gmail.com>
2019-06-03 06:01:38 -0500

    cambios en rama2

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-console.log("hola mundo");
-console.log(" me siento diferente en el dia de hoy")
+console.log("hola mundo22222222");
+console.log(" me siento diferente en el dia de hoy22222222222222222stat")
